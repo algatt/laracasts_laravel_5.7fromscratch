08f8fb2baff92b53b03a15d0521cc9c28209a802
Alan Gatt <alangatt@gmail.com>
2019-04-05 18:01:35 +0200

    pages controller

--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-
-    $tasks = [
-        'Go to the store',
-        'Go to the market',
-        'Go to work',
-        'Go to the concert'
-    ];
-
-    return view('welcome', [
-        'tasks' => $tasks,
-        'foo' => 'foobar'
-    ]);
-});
-
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/', 'PagesController@home');
+Route::get('/contact', 'PagesController@contact');
+Route::get('/about', 'PagesController@about');
